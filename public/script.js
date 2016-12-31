a347c666a7062a380e49e88261a0d475d526847d
Giang Nam <namle@kinssolutions.net>
2016-12-31 13:18:05 +0700

    asd

--- public/script.js
+++ public/script.js
@@ -1,4 +1,3 @@
-// <iframe width="640" height="360" src="https://www.youtube.com/embed/Q3Dnj43MIIU?rel=0&amp;showinfo=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
 
 $(document).ready(function() {
   $(".video-action").click(function() {
@@ -12,5 +11,26 @@ $(document).ready(function() {
     $(".form").removeClass("active");
     $(".form-" + id).addClass("active");
     $(".shop .title").text($(".form-" + id).attr("pname"));
+  });
+
+  $(".nav-0").click(function() {
+    $('html, body').animate({
+      scrollTop: $(".video").offset().top - 50
+    }, 500);
+  });
+  $(".nav-1").click(function() {
+    $('html, body').animate({
+      scrollTop: $(".features").offset().top - 50
+    }, 500);
+  });
+  $(".nav-2").click(function() {
+    $('html, body').animate({
+      scrollTop: $(".media").offset().top - 50
+    }, 500);
+  });
+  $(".nav-3").click(function() {
+    $('html, body').animate({
+      scrollTop: $(".shop").offset().top - 50
+    }, 500);
   });
 });
