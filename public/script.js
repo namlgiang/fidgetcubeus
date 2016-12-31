ecfc90f70dc95c1a5a9736221f2870bc83c09470
Giang Nam <namle@kinssolutions.net>
2016-12-31 16:47:27 +0700

    asd

--- public/script.js
+++ public/script.js
@@ -1,3 +1,12 @@
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-44699912-27', 'auto');
+ga('require', 'displayfeatures');
+ga('send', 'pageview');
+
 
 $(document).ready(function() {
   $(".video-action").click(function() {
@@ -32,5 +41,11 @@ $(document).ready(function() {
     $('html, body').animate({
       scrollTop: $(".shop").offset().top - 50
     }, 500);
+  });
+
+  $("form").submit(function() {
+    fbq('track', 'InitiateCheckout');
+    ga('send', 'pageview', 'InitiateCheckout');
+    console.log("Track Init Check Out");
   });
 });
